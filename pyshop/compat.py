8228a9e28e49dd75d7e55d86989815d9bfd33ad7
Colin Goutte <cgte@bk.ru>
2020-04-02 10:28:45 +0200

    Add compatibility for pypy and pypy3 in tests

--- pyshop/compat.py
+++ pyshop/compat.py
@@ -1,20 +1,27 @@
 import sys
+import platform
 
+pypy = platform.python_implementation() == "PyPy"
 
-if sys.version < '3':
-
-     unicode = __builtins__['unicode']
-     bytes = str
-     from cStringIO import StringIO
+if sys.version < "3":
+    if pypy:
+        unicode = __builtins__.unicode
+    else:
+        unicode = __builtins__["unicode"]
+    bytes = str
+    from cStringIO import StringIO
 
 else:
-     unicode = str
-     bytes = __builtins__['bytes']
+    if pypy:
+        __builtins__.bytes
+    else:
+        bytes = __builtins__["bytes"]
 
-     from io import StringIO
+    unicode = str
+    from io import StringIO
 
 
 def to_unicode(text):
     if isinstance(text, bytes):
-        return text.decode('utf-8')
+        return text.decode("utf-8")
     return text
